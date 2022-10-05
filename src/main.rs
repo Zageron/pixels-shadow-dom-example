71fb1dde4840045aaa1bafce7a33118a9bc38297
Zageron <hello@zageron.com>
2022-10-04 19:30:24 -0700

    feat: figured out a workaround for shadow dom

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 #![deny(clippy::all)]
 #![forbid(unsafe_code)]
 
+use log::error;
 use pixels::{Pixels, SurfaceTexture};
 use std::rc::Rc;
 use winit::dpi::LogicalSize;
-use winit::event_loop::EventLoop;
+use winit::event::{Event, VirtualKeyCode};
+use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::WindowBuilder;
+use winit_input_helper::WinitInputHelper;
+
+const WIDTH: u32 = 320;
+const HEIGHT: u32 = 240;
+const BOX_SIZE: i16 = 64;
+
+/// Representation of the application state. In this example, a box will bounce around the screen.
+struct World {
+    box_x: i16,
+    box_y: i16,
+    velocity_x: i16,
+    velocity_y: i16,
+}
 
 #[cfg(target_arch = "wasm32")]
 use web_sys::HtmlCanvasElement;
@@ -44,16 +59,97 @@ async fn run(canvas: Option<HtmlCanvasElement>) {
 
     let window = Rc::new(window);
 
-    let _pixels = {
+    let mut input = WinitInputHelper::new();
+    let mut pixels = {
         let window_size = window.inner_size();
-
         let surface_texture =
             SurfaceTexture::new(window_size.width, window_size.height, window.as_ref());
-
-        Pixels::new_async(canv.width(), canv.height(), surface_texture)
+        Pixels::new_async(WIDTH, HEIGHT, surface_texture)
             .await
             .expect("Pixels error")
     };
+    let mut world = World::new();
+
+    event_loop.run(move |event, _, control_flow| {
+        // Draw the current frame
+        if let Event::RedrawRequested(_) = event {
+            world.draw(pixels.get_frame_mut());
+            if pixels
+                .render()
+                .map_err(|e| error!("pixels.render() failed: {}", e))
+                .is_err()
+            {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+        }
+
+        // Handle input events
+        if input.update(&event) {
+            // Close events
+            if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+
+            // Resize the window
+            if let Some(size) = input.window_resized() {
+                pixels.resize_surface(size.width, size.height);
+            }
+
+            // Update internal state and request a redraw
+            world.update();
+            window.request_redraw();
+        }
+    });
+}
+
+impl World {
+    /// Create a new `World` instance that can draw a moving box.
+    fn new() -> Self {
+        Self {
+            box_x: 24,
+            box_y: 16,
+            velocity_x: 1,
+            velocity_y: 1,
+        }
+    }
+
+    /// Update the `World` internal state; bounce the box around the screen.
+    fn update(&mut self) {
+        if self.box_x <= 0 || self.box_x + BOX_SIZE > WIDTH as i16 {
+            self.velocity_x *= -1;
+        }
+        if self.box_y <= 0 || self.box_y + BOX_SIZE > HEIGHT as i16 {
+            self.velocity_y *= -1;
+        }
+
+        self.box_x += self.velocity_x;
+        self.box_y += self.velocity_y;
+    }
+
+    /// Draw the `World` state to the frame buffer.
+    ///
+    /// Assumes the default texture format: `wgpu::TextureFormat::Rgba8UnormSrgb`
+    fn draw(&self, frame: &mut [u8]) {
+        for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
+            let x = (i % WIDTH as usize) as i16;
+            let y = (i / WIDTH as usize) as i16;
+
+            let inside_the_box = x >= self.box_x
+                && x < self.box_x + BOX_SIZE
+                && y >= self.box_y
+                && y < self.box_y + BOX_SIZE;
+
+            let rgba = if inside_the_box {
+                [0x5e, 0x48, 0xe8, 0xff]
+            } else {
+                [0x48, 0xb2, 0xe8, 0xff]
+            };
+
+            pixel.copy_from_slice(&rgba);
+        }
+    }
 }
 
 fn main() {
--- web/index.js
+++ web/index.js
@@ -1,16 +1,23 @@
 import init, { initialize, play } from "./bin/pixels-shadow-dom-example.js";
 
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 async function main() {
     await init();
     const shadow = document.body.attachShadow({ mode: 'open' });
 
-    let canvas = shadow.appendChild(document.createElement("canvas"));
+    let canvas = document.body.appendChild(document.createElement("canvas"));
     canvas.setAttribute('class', 'canvas');
     canvas.width = 320;
     canvas.height = 240;
 
-    shadow.appendChild(document.createElement('p'));
     play(canvas);
+
+    await delay(1);
+
+    document.body.removeChild(canvas);
+    let _shadow_canvas = shadow.appendChild(canvas);
+    shadow.appendChild(document.createElement('p'));
 }
 
 main();
